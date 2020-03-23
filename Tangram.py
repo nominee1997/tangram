757adfb9f4d954510818962a1cebcc9bbb4992f1
Aatu Alatervo <aatu.alatervo@hotmail.fi>
2020-03-23 13:46:21 +0200

    Submitted work

--- Tangram.py
+++ Tangram.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 13 21:50:07 2020
-
-@author: Admin
-"""
 from typing import List, Tuple
 from itertools import combinations_with_replacement, permutations, product
 from collections import defaultdict as ddict
@@ -14,20 +9,21 @@ import time
 
 def angle_combinations(angles: List[int]) -> List[Tuple[int]]:
     """
-    Generate all combinations of angles 45, 90, 135 which can create an n-gon
-    with n <= 8.
+    Generate all combinations of angles which can create an n-gon
+    with n <= 8. angles should contain 180.
     """
     combinations = set()
     angle_sum = 180*6       # Sum of angles 180*(n-2), n=8
     for combination in combinations_with_replacement(angles, 8):
         if sum(combination) == angle_sum:
+            # Sort angles and remove 180 angles
             combination = tuple(sorted(filter(lambda x: x != 180, combination)))
             combinations.add(combination)
     return sorted(list(combinations), key=lambda x: len(x))     # Sort by n
 
 def angle_unique_cycles(combination: Tuple[int]) -> List[Tuple[int]]:
     """
-    Generate all permutations and remove extra cycles
+    Generate all permutations and remove non-unique cycles
     Note: Inefficient algorithm, but ok since small size
     """
     cycles = []
@@ -57,9 +53,7 @@ def polygon_area(x: Quadratic, y: Quadratic) -> Quadratic:
 def simulation(cycle: Tuple[int], dist: List[Quadratic]) -> List[List[Quadratic]]:
     """
     Runs the simulation on a specific cycle. All possible distances between 
-    angles (given in dist) are tested. Could be improved quite a lot by using 
-    a backtracking approach, but we can get away with an inefficient approach
-    since n is quite small.
+    angles (given in dist) are tested. 
     """
     # Generate sin and cos functions
     sin = { 0: Quadratic(0),     45: Quadratic(0, 0.5),
@@ -69,44 +63,32 @@ def simulation(cycle: Tuple[int], dist: List[Quadratic]) -> List[List[Quadratic]
     cos = {}
     for key, val in sin.items():
         cos[(key+270)%360] = val
-    
     # Run the simulation
     res = []
     for d in product(dist, repeat=len(cycle)):
         for rational in [True, False]:
-            simulation_step(res, sin, cos, d, rational)
+            # Define starting point
+            x, y = Quadratic(1), Quadratic(0)
+            cur_angle = 0
+            # Generate shape
+            X, Y = [x], [y]
+            for i, angle in enumerate(cycle):
+                if angle in [45, 135]:
+                    rational = not rational
+                if rational:
+                    dst = Quadratic(d[i])
+                else:
+                    dst = Quadratic(0, 0.5*d[i])
+                x += dst*cos[(cur_angle+180-angle)%360]
+                y += dst*sin[(cur_angle+180-angle)%360]                
+                X.append(x)
+                Y.append(y)
+                cur_angle += 180-angle
+            # Valid shape will have area = 1, and will end where we started!    
+            if polygon_area(X, Y) == Quadratic(1) and x == Quadratic(1)  \
+                                                        and y == Quadratic(0):
+                res.append([X, Y])
     return res
-
-def simulation_step(res, sin, cos, d, rational) -> None:
-    """
-    Runs the simulation for one step.
-    """
-    # Define starting point
-    x, y = Quadratic(1), Quadratic(0)
-    cur_angle = 0
-    # Generate shape
-    X, Y = [x], [y]
-    for i, angle in enumerate(cycle):
-        if angle in [45, 135]:
-            rational = not rational
-        if rational:
-            dst = Quadratic(d[i])
-        else:
-            dst = Quadratic(0, 0.5*d[i])
-        x += dst*cos[(cur_angle+180-angle)%360]
-        y += dst*sin[(cur_angle+180-angle)%360]
-        
-        # Terminate early by convexity
-        if y < Quadratic(0):
-            break
-        
-        X.append(x)
-        Y.append(y)
-        cur_angle += 180-angle
-    # Valid shape will have area = 1, and will end where we started!    
-    if polygon_area(X, Y) == Quadratic(1) and x == Quadratic(1)  \
-                                                and y == Quadratic(0):
-        res.append([X, Y])
 
 def graph_results():
     """
@@ -122,6 +104,7 @@ def graph_results():
         ax.label_outer()
         ax.axis('scaled')
     fig.show()
+    
 
 if __name__ == "__main__":
     # Find possible combinations on angles which still form a valid n-gon
